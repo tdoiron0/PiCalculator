fd700c84bc439bb5eb89ac51da95058846700b38
tdoiron0 <trentwdoiron@gmail.com>
2024-05-27 20:27:53 -0400

    Added symbolic algebra library

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import symjava.symbolic.Expr;
+import symjava.symbolic.Func;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
