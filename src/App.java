7399b79032b76ec605bb5522f4d03c5f16814ff3
tdoiron0 <trentwdoiron@gmail.com>
2024-05-31 22:11:36 -0400

    Deleted symbolics

--- src/App.java
+++ src/App.java
@@ -1,19 +1,8 @@
 import java.util.Random;
 
-import Symbolics.Number;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        /* 
-        for (int i = 0; i < 100; ++i) {
-            System.out.println(genRandNumber());
-        } 
-        */
-
-        Number num1 = new Number(genRandNumber());
-        Number num2 = new Number(genRandNumber());
-
-        System.out.println(num1 + " + " + num2 + " = " + num1.add(num2));
+        
     }
 
     public static String genRandNumber() {
--- src/Symbolics/Member.java
+++
@@ -1,8 +0,0 @@
-package Symbolics;
-
-public interface Member {
-    public abstract Member add(Member oper);
-    public abstract Member sub(Member oper);
-    public abstract Member mult(Member oper);
-    public abstract Member divi(Member oper);
-}
