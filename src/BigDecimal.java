75e527d58720752cd91ba0bcfd86b0e938b4df63
Trent Doiron <103758611+tdoiron0@users.noreply.github.com>
2024-06-11 00:57:52 -0400

    ...

--- src/BigDecimal.java
+++ src/BigDecimal.java
@@ -4,7 +4,11 @@ import java.util.LinkedList;
 public class BigDecimal {
     public static final int MAX_BLOCK_SIZE = 100;
 
-    public class Block {
+    private boolean negative = false;
+    private int decimalPlace = 0;
+    private final LinkedList<Block> blocks = new LinkedList<>();
+
+    private class Block {
         private final ArrayList<Integer> digits;
         private String filePath = "";
 
@@ -16,18 +20,71 @@ public class BigDecimal {
             this.filePath = filePath;
         }
 
-        public Object[] add(Block oper) {
-            Object[] result = { new Block(filePath), 0};
+        public int digitLength() { return digits.size(); }
+        public Integer getDigit(int index) { return digits.get(index); }
+
+        public Object[] add(Block oper, int prevCarry) {
             ArrayList<Integer> resultDigits = new ArrayList<>();
+
+            int i = digits.size() - 1;
+            int j = oper.digitLength() - 1;
+            int carry = prevCarry;
+            while (i >= 0 && j >= 0) {
+                int total = digits.get(i) + oper.getDigit(j) + carry;
+                int digit = total % 10;
+                carry = total / 10;
+                resultDigits.add(digit);
+
+                --i;
+                --j;
+            }
+
+            while (i >= 0) {
+                int total = digits.get(i) + carry;
+                int digit = total % 10; 
+                carry = total / 10;
+                resultDigits.add(digit);
+                --i;
+            }
+            while (j >= 0) {
+                int total = oper.getDigit(j) + carry;
+                int digit = total % 10; 
+                carry = total / 10;
+                resultDigits.add(digit);
+                --j;
+            }
             
-            
+            Object[] result = { new Block(filePath), carry };
             return result;
         }
     }
-    
-    private final ArrayList<Block> blocks = new ArrayList<>();
 
     public BigDecimal(BigDecimal.Block block) {
-        blocks.add(block);
+        blocks.addFirst(block);
+    }
+
+    public boolean isNegative() { return negative; }
+    public int getDecimalPlace() { return decimalPlace; } 
+    public int getBlockSize() { return blocks.size(); }
+    public Block getBlock(int index) { return blocks.get(index); }
+
+    public BigDecimal add(BigDecimal oper) {
+        LinkedList<Block> resultBlocks = new LinkedList<>();
+
+        int i = blocks.size() - 1;
+        int j = oper.getBlockSize() - 1;
+        int prevCarry = 0;
+        while (i >= 0 && j >= 0) {
+            Object[] block = blocks.get(i).add(oper.getBlock(j), prevCarry);
+            resultBlocks.addFirst((BigDecimal.Block)block[0]);
+            prevCarry = (Integer)block[1];
+
+            --i;
+            --j;
+        }
+
+        
+
+        return null;
     }
 }
