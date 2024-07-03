bd29e7d375e61b300a13f7fb0000d64751532684
Trent Doiron <103758611+tdoiron0@users.noreply.github.com>
2024-07-02 23:57:56 -0400

    rewrote code to not use blocks anymore

--- src/BlockDecimal.java
+++ src/BlockDecimal.java
@@ -1,218 +1,142 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.IOError;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class BlockDecimal {
     public static final int MAX_BLOCK_SIZE = 100;
     public static final String MATH_ENV_FILE_PATH = "testdata/";
 
-    private boolean negative = false;
-    private int decimalPlace = 0;
-    private File folder;
-    private LinkedList<Block> blocks = new LinkedList<>();
-
-    private class Block {
-        private List<Integer> digits;
-
-        public Block(List<Integer> digits) {
-            this.digits = digits;
-        }
-        private Block(int startValue) {
-            digits = new ArrayList<>();
-            digits.addFirst(startValue);
-        }
-
-        public int digitLength() { return digits.size(); }
-        public Integer getDigit(int index) { return digits.get(index); }
-
-        public Object[] add(Block oper, int prevCarry) {
-            ArrayList<Integer> resultDigits = new ArrayList<>();
-
-            int i = digits.size() - 1;
-            int j = oper.digitLength() - 1;
-            int carry = prevCarry;
-            while (i >= 0 && j >= 0) {
-                int total = digits.get(i) + oper.getDigit(j) + carry;
-                int digit = total % 10;
-                carry = total / 10;
-                resultDigits.addFirst(digit);
-
-                --i;
-                --j;
-            }
-
-            while (i >= 0) {
-                int total = digits.get(i) + carry;
-                int digit = total % 10; 
-                carry = total / 10;
-                resultDigits.addFirst(digit);
-                --i;
-            }
-            while (j >= 0) {
-                int total = oper.getDigit(j) + carry;
-                int digit = total % 10; 
-                carry = total / 10;
-                resultDigits.addFirst(digit);
-                --j;
-            }
-
-            if (carry != 0 && resultDigits.size() < MAX_BLOCK_SIZE) {
-                resultDigits.addFirst(carry);
-                Object[] result = { new Block(resultDigits), 0 };
-                return result;
-            } else {
-                Object[] result = { new Block(resultDigits), carry };
-                return result;
-            }
-        }
-        public void open(String filePath) throws IOException {
-            if (filePath.length() == 0) {
-                throw new IllegalStateException("Must set filepath before opening file");
-            }
-
-            File file = new File(filePath);
-            String data = Files.readString(file.toPath());
-            String[] dataParsed = data.substring(1, data.length() - 1).split(", ");
-            for (String it : dataParsed) {
-                digits.add(Integer.parseInt(it));
-            }
-        }
-        public void close() {
-            digits = new ArrayList<>();
-        }
-        public void write(String filePath) throws IOException {
-            File file = new File(filePath);
-            if (!file.exists()) {
-                file.getParentFile().mkdir();
-                file.createNewFile();
-            } else {
-                file.delete();
-                file.getParentFile().mkdir();
-                file.createNewFile();
-            }
-            Files.writeString(new File(filePath).toPath(), digits.toString(), StandardOpenOption.CREATE);
-        }
-
-        public void print(String filePath) {
-            try {
-                open(filePath);
-                StringBuilder sb = new StringBuilder();
-                for (Integer it : digits) {
-                    sb.append(it);
-                }
-                System.out.print(sb.toString());
-            } catch (IOException e) {
-                System.out.println("ERROR::failed to open file to print:\n" + e);
-            }
-        }
-    }
+    private File folder = null;
+    private int numBlocks = 0;
 
     public BlockDecimal(String src) {
-        folder = new File(MATH_ENV_FILE_PATH + MathEnvironment.getId());
-        
-        List<List<Integer>> digits = new ArrayList<>();
-        List<Integer> currList = new ArrayList<>();
+        this.folder = new File(MATH_ENV_FILE_PATH + MathEnvironment.getId());
+
         for (int i = src.length() - 1; i >= 0; --i) {
-            currList.addFirst(src.charAt(i) - 48);
-            if ((i + 1) % MAX_BLOCK_SIZE == 0) {
-                digits.addFirst(currList);
-                currList = new ArrayList<>();
-            }
-        }
-        if (currList.size() > 0) {
-            digits.addFirst(currList);
-        }
-
-        for (int i = digits.size() - 1; i >= 0; --i) {
-            Block newBlock = new Block(digits.get(i));
-            try {
-                newBlock.write(folder.getAbsolutePath() + "/" + (i + 1) +".txt");
-            } catch (IOException e) {
-                System.out.println("ERROR::failed to create decimal:\n" + e);
-            }
-            newBlock.close();
-
-            this.blocks.addFirst(newBlock);
-        }
-    }
-    private BlockDecimal(LinkedList<Block> blocks, File folder) {
-        this.blocks = blocks;
-        this.folder = folder;
+            addDigits(src.charAt(i) - 48);
+        } 
     }
 
-    public boolean isNegative() { return negative; }
-    public int getDecimalPlace() { return decimalPlace; } 
-    public int getBlockSize() { return blocks.size(); }
-    public Block getBlock(int index) { return blocks.get(index); }
-    public File getFolder() { return folder; }
+    public int getNumBlocks() { return numBlocks; } 
 
     public BlockDecimal add(BlockDecimal oper) {
-        LinkedList<Block> blocksResult = new LinkedList<>();
-        File resultFolder = new File(MATH_ENV_FILE_PATH + MathEnvironment.getId());
-
-        int i = blocks.size() - 1;
-        int j = oper.getBlockSize() - 1;
-        int prevCarry = 0;
+        BlockDecimal result = new BlockDecimal("");
+        int i = numBlocks - 1;
+        int j = oper.getNumBlocks() - 1;
+        int carry = 0;
         while (i >= 0 && j >= 0) {
             try {
-                blocks.get(i).open(folder.getAbsolutePath() + "/" + (i + 1) + ".txt");
-                oper.getBlock(j).open(oper.getFolder().getAbsolutePath() + "/" + (j + 1) + ".txt");
-                
-                Object[] temp = blocks.get(i).add(oper.getBlock(j), prevCarry);
-                blocksResult.addFirst((BlockDecimal.Block)temp[0]);
-                prevCarry = (Integer)temp[1];
+                List<Integer> block1 = getBlock(i);
+                List<Integer> block2 = oper.getBlock(i);
 
-                blocksResult.getFirst().write(resultFolder.getAbsolutePath() + "/" + (Math.max(i, j) + 1) + ".txt");
+                Object[] opRes = addBlock(block1, block2, carry);
+                List<Integer> newDigits = (List<Integer>)opRes[0];
+                carry = (Integer)opRes[1];
 
-                blocks.get(i).close();
-                oper.getBlock(j).close();
-                blocksResult.getFirst().close();
+                result.addDigits(newDigits);
             } catch (IOException e) {
                 System.out.println("Addition failed:\n" + e.toString());
                 return null;
             }
+        }
+        return result;
+    }
+    public void print() {
+        
+    }
+
+    private Object[] addBlock(List<Integer> oper1, List<Integer> oper2, int prevCarry) {
+        ArrayList<Integer> resultDigits = new ArrayList<>();
+
+        int i = oper1.size() - 1;
+        int j = oper2.size() - 1;
+        int carry = prevCarry;
+        while (i >= 0 && j >= 0) {
+            int total = oper1.get(i) + oper2.get(j) + carry;
+            int digit = total % 10;
+            carry = total / 10;
+            resultDigits.addFirst(digit);
 
             --i;
             --j;
         }
 
-        Block carryBlock = new Block(prevCarry);
         while (i >= 0) {
-            Object[] temp = blocks.get(i).add(carryBlock, 0);
-            blocksResult.add((BlockDecimal.Block)temp[0]);
-            carryBlock = new Block((Integer)temp[1]);
+            int total = oper1.get(i) + carry;
+            int digit = total % 10; 
+            carry = total / 10;
+            resultDigits.addFirst(digit);
             --i;
         }
         while (j >= 0) {
-            Object[] temp = oper.getBlock(j).add(carryBlock, 0);
-            blocksResult.add((BlockDecimal.Block)temp[0]);
-            carryBlock = new Block((Integer)temp[1]);
+            int total = oper2.get(j) + carry;
+            int digit = total % 10; 
+            carry = total / 10;
+            resultDigits.addFirst(digit);
             --j;
         }
-        
-        if (carryBlock.digitLength() != 0) {
-            blocks.addFirst(carryBlock);
+
+        if (carry == 0 && resultDigits.size() < MAX_BLOCK_SIZE) {
+            resultDigits.addFirst(carry);
+            Object[] result = { resultDigits, 0 };
+            return result;
+        } else {
+            Object[] result = { resultDigits, carry };
+            return result;
+        }
+    }
+    public List<Integer> getBlock(int index) throws IOException{
+        String data = Files.readString(Paths.get(blockPath(index)));
+        String[] dataParsed = data.substring(1, data.length() - 1).split(", ");
+        List<Integer> result = new ArrayList<>();
+        for (String it : dataParsed) {
+            result.add(Integer.parseInt(it));
+        }
+        return result;
+    }
+    private void writeBlock(List<Integer> data, int index) throws IOException {
+        File file = new File(blockPath(index));
+        if (!file.exists()) {
+            file.getParentFile().mkdir();
+            file.createNewFile();
+        } else {
+            file.delete();
+            file.createNewFile();
         }
 
-        return new BlockDecimal(blocksResult, resultFolder);
+        Files.writeString(file.toPath(), data.toString(), StandardOpenOption.CREATE);
     }
-    public void cleanUp() {
-
-    }
-
-    public void print() {
-        System.out.println("folder path: " + folder.getAbsolutePath());
-        System.out.println("num blocks: " + blocks.size());
-        System.out.print("Digits: ");
-        for (int i = 1; i <= blocks.size(); ++i) {
-            blocks.get(i - 1).print(folder.getAbsolutePath() + "/" + i + ".txt");
+    private void addDigits(List<Integer> digits) {
+        try {
+            List<Integer> lastDigits = getBlock(numBlocks - 1);
+            for (int i = digits.size() - 1; i >= 0; --i) {
+                if (lastDigits.size() + 1 == MAX_BLOCK_SIZE) {
+                    writeBlock(lastDigits, numBlocks);
+                    lastDigits = new ArrayList<>();
+                    ++numBlocks;
+                }
+                lastDigits.addFirst(digits.get(i));
+            }
+            if (lastDigits.size() != 0) {
+                writeBlock(lastDigits, numBlocks);
+                ++numBlocks;
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR::failed to add digits to decimal:\n" + e.toString());
+            return;
         }
-        System.out.println("");
+    }
+    private void addDigits(int digit) {
+        List<Integer> list = new ArrayList<>();
+        list.add(digit);
+        addDigits(list);
+    }
+    private String blockPath(int index) {
+        return folder.getAbsolutePath() + "/" + index + ".txt";
     }
 }
